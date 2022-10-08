f3a235acb5347ba28a45e800ba629ff727ea612c
breadan alaa <109834067+breadan@users.noreply.github.com>
2022-10-08 06:34:24 +0200

    W. Mathematical Expression
    الجمع مع التصحيح

---
+++ W MathematicalExpression.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n1, n2, result;
+	char opr,oprres;
+	cin >> n1 >> opr>>n2>>oprres>>result;
+	switch (opr)
+	{
+	case '+':
+		if (n1 + n2 == result) {
+			cout << "YES\n";
+		}
+		else {
+			cout << n1 + n2;
+		}
+		break;
+	case '-':
+		if (n1 - n2 == result) {
+			cout << "YES\n";
+		}
+		else {
+			cout << "NO";
+		}
+		break;
+	case '*':
+		if (n1 * n2 == result) {
+			cout << "YES\n";
+		}
+		else {
+			cout << n1 * n2;
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	return 0;
+}
