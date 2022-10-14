c274061048265328b69ad1fffb5ccf56a27baa92
breadan alaa <109834067+breadan@users.noreply.github.com>
2022-10-14 12:32:37 -0700

    A. Watermelon
    https://codeforces.com/problemset/problem/4/A

---
+++ waterMelone.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int w;
+	cin >> w;
+	if (w % 2 == 1) {
+		cout << "No";
+	}
+	else {
+		if (w == 2) {
+			cout << "NO";
+		}
+		else {
+			cout << "YES";
+		}
+	}
+	
+	
+
+	return 0;
+}
