577f16145ec086729604f42f21a8d431e4e1802a
breadan alaa <109834067+breadan@users.noreply.github.com>
2022-10-15 21:10:02 -0700

    A. Next Round
    https://codeforces.com/problemset/problem/158/A

---
+++ nextRound.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	 int n, k, count =0;
+	 cin >> n >> k;
+	 int arr[100];//بتعمل رقم كبير المهم
+	 //استقبال الارقام من المستخدم
+	 for (int i = 1; i <= n; i++) {
+		 cin >> arr[i];
+	 }
+	 for (int i = 1; i <= n; i++) {
+		 if (arr[i] >= arr[k] && arr[i] != 0) {
+			 count++;
+		 }
+	 }cout << count;
+
+
+
+
+	return 0;
+}
